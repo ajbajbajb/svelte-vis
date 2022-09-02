3e1003e86f680d40e661b3fc6127cdb3e5f1dc41
ajbajbajb <36610573+ajbajbajb@users.noreply.github.com>
2022-09-02 18:09:25 +0200

    lesson 3, remembering...

--- src/03-scatter.svelte
+++ src/03-scatter.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-  import Bar from "./02-bar.svelte";
   //  importing both of these functions because we don't know the data yet.
-  import { scaleLinear } from "d3-scale";
+  import { scaleLinear, scaleOrdinal } from "d3-scale";
   import { extent } from "d3-array";
+  const height = 600;
+  const width = 600;
+  const buffer = 10;
+  const axisSpace = 30;
+  const colors = ["red", "green", "blue"];
 
   interface IrisEntry {
     sepalLength: number;
@@ -14,10 +18,33 @@
   async function loadData() {
     let data = await fetch("/data/iris.json");
     let json = (await data.json()) as IrisEntry[];
+
     // console.log(json);
     let xExtent = extent(json, (d) => d.petalLength);
+    let yExtent = extent(json, (d) => d.petalWidth);
     console.log(xExtent);
-    return json;
+
+    // javascript set that is passed a list of each flower.
+    let species = new Set(json.map((d) => d.species));
+    console.log(species);
+
+    let colorScale = scaleOrdinal().domain(Array.from(species)).range(colors);
+    // construct the scale by interpolating between the two values identified in the extent
+    // and then restrict them to the range of the available width or height.
+    let xScale = scaleLinear()
+      .domain(xExtent)
+      .range([buffer + axisSpace, width - buffer - axisSpace]);
+    let yScale = scaleLinear()
+      .domain(yExtent)
+      .range([height - buffer - axisSpace, buffer + axisSpace]);
+
+    return {
+      data: json,
+      xScale,
+      yScale,
+      colorScale,
+      species: Array.from(species),
+    };
   }
   let data = loadData();
 </script>
@@ -25,5 +52,35 @@
 {#await data}
   <p>Loading...</p>
 {:then iris}
-  <p>Loaded.</p>
+  <svg {height} {width}>
+    {#each iris.data as item}
+      <circle
+        r="3"
+        cx={iris.xScale(item.petalLength)}
+        cy={iris.yScale(item.petalWidth)}
+        fill={iris.colorScale(item.species)}
+      />
+    {/each}
+    {#each iris.xScale.ticks(5) as tick}
+      <g transform={`translate(${iris.xScale(tick)} ${height - 20})`}>
+        <line y1="-5" y2="0" stroke="black" />
+        <text y="20" text-anchor="middle">{tick}</text>
+      </g>
+    {/each}
+    {#each iris.yScale.ticks(5) as tick}
+      <g transform={`translate(0 ${iris.yScale(tick)} )`}>
+        <line x1="35" x2="40" stroke="black" />
+        <text x="30" dominant-baseline="middle" text-anchor="end">{tick}</text>
+      </g>
+    {/each}
+    <g transform={`translate(${width - 100}, ${height - 100})`}>
+      {#each iris.species as species, i}
+        <g transform={`translate(0, ${i * 20})`}>
+          <rect height="10" width="10" fill={iris.colorScale(species)} />
+          <text dominant-baseline="middle" y="5" x="20">{species}</text>
+        </g>
+      {/each}
+    </g>
+  </svg>
+  <!-- <p>Loaded.</p> -->
 {/await}
